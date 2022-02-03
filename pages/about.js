0b077148f4ec55d0e16f447f0373d7f0014d10c2
furip0x <furkanipekci@hotmail.com>
2022-02-03 06:27:49 +0300

    Edit user object

--- pages/about.js
+++ pages/about.js
@@ -4,30 +4,32 @@ import Toolbar from "../components/toolbar"
 import styles from "../styles/About.module.css"
 
 const About = ({ user }) => {
-  console.log(user)
   return (
     <>
       <Head>
         <title>About</title>
-        <meta name="description" content={user.description} />
+        <meta name="description" content={user.data.description} />
 
-        <meta property="og:image" content={user.image} />
-        <meta property="og:title" content={`Created by ${user.name}`} />
-        <meta property="og:description" content={user.description} />
+        <meta property="og:image" content={user.data.image} />
+        <meta property="og:title" content={`Created by ${user.data.name}`} />
+        <meta property="og:description" content={user.data.description} />
 
-        <meta property="twitter:image" content={user.image} />
-        <meta property="twitter:title" content={`Created by ${user.name}`} />
-        <meta property="twitter:description" content={user.description} />
+        <meta property="twitter:image" content={user.data.image} />
+        <meta
+          property="twitter:title"
+          content={`Created by ${user.data.name}`}
+        />
+        <meta property="twitter:description" content={user.data.description} />
       </Head>
       <div className="page-container">
         <Toolbar />
         <div className={styles.main}>
           <h1>NextJS News App</h1>
           <div className={styles.about}>
-            <h2>{user.name}</h2>
-            <h6>{user.position}</h6>
-            <img src={user.image} />
-            <p>{user.description}</p>
+            <h2>{user.data.name}</h2>
+            <h6>{user.data.position}</h6>
+            <img src={user.data.image} />
+            <p>{user.data.description}</p>
           </div>
         </div>
       </div>
@@ -36,7 +38,9 @@ const About = ({ user }) => {
 }
 
 export const getServerSideProps = async (pageContext) => {
-  const apiResponse = await fetch("link-here")
+  const apiResponse = await fetch(
+    "https://raw.githubusercontent.com/furip0x/nextjs-news-app/main/db.json"
+  )
 
   const user = await apiResponse.json()
 
